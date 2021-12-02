35ac945aef02f2feee86449177cec8969427577b
Wojciech Kasperski <wojciech.kasperski@allegro.pl>
2021-12-02 15:25:23 +0100

    Add workflow & bluetooth connection

--- put-rocket-frontend/src/components/PairingButton.vue
+++ put-rocket-frontend/src/components/PairingButton.vue
@@ -5,8 +5,10 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Connect & Send Bluetooth Event
         </h1>
-
-        <v-btn color="primary" @click="sendEvent">DO IT, PAIR ME!</v-btn>
+        <v-btn v-if="{ paired }" color="primary" @click="connectToDevice">
+          DO IT, PAIR ME!
+        </v-btn>
+        <p v-else>Device paired successfully 👌 !</p>
       </v-col>
     </v-row>
   </v-container>
@@ -15,10 +17,45 @@
 <script>
 export default {
   name: "PairingButton",
-  data: () => ({}),
+  data: () => ({
+    paired: false,
+  }),
   methods: {
-    sendEvent() {
-      this.$store.dispatch("sendEvent");
+    connectToDevice() {
+      navigator.bluetooth
+        .requestDevice({
+          filters: [
+            {
+              services: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b"],
+            },
+          ],
+        })
+        .then((device) => {
+          console.log('"' + device.name + '" bluetooth device selected');
+
+          device.gatt
+            .connect()
+            .then((server) => {
+              return server.getPrimaryService(
+                "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+              );
+            })
+            .then((service) => {
+              console.log("Service found, getting characteristic...");
+
+              service
+                .getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8")
+                .then((characteristic) => {
+                  console.log("Notifications started");
+                  characteristic.writeValue(
+                    new TextEncoder().encode("Dupencja na 3")
+                  );
+                });
+            })
+            .catch((err) => {
+              console.error("Something is not yes", err);
+            });
+        });
     },
   },
 };
--- put-rocket-frontend/vue.config.js
+++ put-rocket-frontend/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  publicPath: "/put-V2-rocket/",
 };
