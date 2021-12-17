0befa59ed001a906f9c8d7c66bcb05865454208a
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2021-12-18 00:57:54 +0100

    BLE Data Recieve 📘  (#5)
    * Frontend Vue+Vuetify initialization
    
    * Add Footer, Toolbar & Drawer, configure store
    
    * Add simple logo
    
    * Add pairing button
    
    * Add workflow & bluetooth connection
    
    * Add CI folders check
    
    * Add CI folders check - v2
    
    * Add CI folders check - v3
    
    * Add CI folders check - v4
    
    * Add CI folders check - v5
    
    * Add CI folders check - v6 - node bump
    
    * Add CI folders check - v7
    
    * Add CI folders check - v8
    
    * Add base route
    
    * Add PSI API download
    
    * Add BLE data reciving

--- put-rocket-frontend/src/components/PairingButton.vue
+++ put-rocket-frontend/src/components/PairingButton.vue
@@ -5,10 +5,21 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Connect & Send Bluetooth Event
         </h1>
-        <v-btn v-if="{ paired }" color="primary" @click="connectToDevice">
+        <v-btn v-if="!paired" color="primary" @click="connectToDevice">
           DO IT, PAIR ME!
         </v-btn>
-        <p v-else>Device paired successfully 👌 !</p>
+        <div v-else>
+          <p>Device paired successfully 👌 !</p>
+          <ul class="text-left">
+            <li v-for="item in bleData" :key="item.time">
+              Time: {{ item.time }}; Altitude: {{ item.altitude }}; Pressure:
+              {{ item.pressure }}; a(x,y,z, sqrt): ({{ item.aX }},
+              {{ item.aY }}, {{ item.aZ }}, {{ item.aSqrt }}); g(x,y,z): ({{
+                item.gX
+              }}, {{ item.gY }}, {{ item.gZ }}); Battery: {{ item.battery }};
+            </li>
+          </ul>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -19,6 +30,7 @@ export default {
   name: "PairingButton",
   data: () => ({
     paired: false,
+    bleData: [],
   }),
   methods: {
     connectToDevice() {
@@ -35,51 +47,36 @@ export default {
 
           device.gatt
             .connect()
-            .then((server) => {
-              return server.getPrimaryService(
-                "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+            .then((server) =>
+              server.getPrimaryService("4fafc201-1fb5-459e-8fcc-c5c9c331914b")
+            )
+            .then((service) => {
+              console.log("Connected to service");
+              this.paired = true;
+              return service.getCharacteristic(
+                "beb5483e-36e1-4688-b7f5-ea07361b26a8"
               );
             })
-            .then((service) => {
-              console.log("Service found, getting characteristic...");
-
-              service
-                .getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8")
-                .then((characteristic) => {
-                  console.log("Notifications started");
-                  characteristic.writeValue(
-                    new TextEncoder().encode(this.getPressureFromWeartherAPI())
-                  );
-                });
+            .then(async (characteristic) => {
+              console.log("Connected to characteristic");
+              console.log(characteristic.properties);
+              characteristic.startNotifications().then(() => {
+                characteristic.addEventListener(
+                  "characteristicvaluechanged",
+                  this.handleNotification.bind(this)
+                );
+              });
             })
-            .catch((err) => {
-              console.error("Something is not yes", err);
-            });
+            .catch((error) => console.log(error));
         });
     },
-    getPressureFromWeartherAPI() {
-      navigator.geolocation.getCurrentPosition(
-        (loc) => {
-          console.log(loc);
-          const lat = loc.coords.latitude;
-          const lon = loc.coords.longitude;
-          fetch(
-            `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`
-          )
-            .then((response) => {
-              return response.json();
-            })
-            .then((response) => {
-              const pressure = response.main.pressure;
-              console.log("Pressure is: ", pressure, response);
-              return pressure;
-            });
-        },
-        () => {
-          console.error("Sorry dude, BT not works!");
-        }
+    handleNotification(event) {
+      const value = event.target.value.buffer;
+      const recivedData = JSON.parse(
+        String.fromCharCode.apply(null, new Uint8Array(value))
       );
-      return "No weather data available!";
+      console.log("Received ", recivedData);
+      this.bleData.push(recivedData);
     },
   },
 };
