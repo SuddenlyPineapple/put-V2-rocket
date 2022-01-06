ee9ec6df780ea2de7422b23348b4bedfff4c1536
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2022-01-06 17:40:11 +0100

    Add charts after connecting (add vue-ploty dependency) (#9)

---
+++ put-rocket-frontend/src/components/IndexChart.vue
@@ -0,0 +1,68 @@
+<template>
+  <Plotly :data="chartData" :layout="layout" :display-mode-bar="false"></Plotly>
+</template>
+
+<script>
+import { Plotly } from "vue-plotly";
+
+export default {
+  name: "IndexChart",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    yLabel: {
+      type: String,
+      required: false,
+      default: "Y Axis",
+    },
+    xLabel: {
+      type: String,
+      required: false,
+      default: "X Axis",
+    },
+    xData: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    yData: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  components: {
+    Plotly,
+  },
+  computed: {
+    chartData: function () {
+      return [
+        {
+          x: this.yData,
+          y: this.xData,
+          type: "scatter",
+        },
+      ];
+    },
+    layout: function () {
+      return {
+        title: {
+          text: this.name,
+        },
+        xaxis: {
+          title: {
+            text: this.yLabel,
+          },
+        },
+        yaxis: {
+          title: {
+            text: this.xLabel,
+          },
+        },
+      };
+    },
+  },
+};
+</script>
